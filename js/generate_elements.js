3831475e76dd17274db2e7e405717161cb594bd1
Noah Kaplan <noahkaplan4@gmail.com>
2014-10-14 22:14:31 -0700

    round coordinates to one decimal

--- js/generate_elements.js
+++ js/generate_elements.js
@@ -8,8 +8,8 @@ function generateRect(x, y, width, height, borderRadius, fill, stroke, strokeWid
     stroke = stroke || defaultDarkColor;
     strokeWidth = strokeWidth   || 1;
     var el = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
-    el.setAttribute('x', x);
-    el.setAttribute('y', y);
+    el.setAttribute('x', Math.round(x*10) / 10);
+    el.setAttribute('y', Math.round(y*10) / 10);
     el.setAttribute('width', width);
     el.setAttribute('height', height);
     el.setAttribute('rx', borderRadius);
@@ -27,8 +27,8 @@ function generateCircle(cx, cy, r, fill, stroke, strokeWidth){
     stroke = stroke || defaultDarkColor;
     strokeWidth = strokeWidth || 1;
     var el = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
-    el.setAttribute('cx', cx);
-    el.setAttribute('cy', cy);
+    el.setAttribute('cx', Math.round(cx*10) / 10);
+    el.setAttribute('cy', Math.round(cy*10) / 10);
     el.setAttribute('r', r);
     el.setAttribute('fill', fill);
     el.setAttribute('stroke', stroke);
@@ -54,10 +54,10 @@ function generateLine(x1, y1, x2, y2, stroke, strokeWidth){
     stroke = stroke || '#999';
     strokeWidth = strokeWidth || 1;
     var el = document.createElementNS('http://www.w3.org/2000/svg', 'line');
-    el.setAttribute('x1', x1);
-    el.setAttribute('y1', y1);
-    el.setAttribute('x2', x2);
-    el.setAttribute('y2', y2);
+    el.setAttribute('x1', Math.round(x1*10) / 10);
+    el.setAttribute('y1', Math.round(y1*10) / 10);
+    el.setAttribute('x2', Math.round(x2*10) / 10);
+    el.setAttribute('y2', Math.round(y2*10) / 10);
     el.setAttribute('stroke', stroke);
     el.setAttribute('stroke-width', strokeWidth);
     svg.append(el);
