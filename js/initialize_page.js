a6e2b7be4064ca9ac4639e56d04d705f42ed2652
Noah Kaplan <noahkaplan4@gmail.com>
2014-10-14 22:02:57 -0700

    added Open Sans

--- js/initialize_page.js
+++ js/initialize_page.js
@@ -1,6 +1,7 @@
 window.onload = function(){
 
-	svg.attr({'width': svgWidth, 'height': svgHeight, 'style': 'background:rgb(247, 247, 247)'});
+	svg.attr({'width': svgWidth, 'height': svgHeight});
 	document.body.style.backgroundColor = pageBackground;
+	document.body.style.fontFamily = 'Open Sans';
 
 };
--- js/static_variables.js
+++ js/static_variables.js
@@ -1,5 +1,4 @@
 var defaultDarkColor  = '#555';
 var defaultLightColor = 'white';
 
-var pageBackground = '#777';
-var svgBackground = 'rgb(247, 247, 247)';
+var pageBackground = 'rgb(247, 247, 247)';
