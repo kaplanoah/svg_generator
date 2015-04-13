11ad8661758e69d50d3b33e3d33d47e1be2afb88
Noah Kaplan <noahkaplan4@gmail.com>
2015-04-13 15:31:03 -0700

    arc path generator

--- js/generate_elements.js
+++ js/generate_elements.js
@@ -8,8 +8,8 @@ function generateRect(x, y, width, height, borderRadius, fill, stroke, strokeWid
     stroke = stroke || defaultDarkColor;
     if ( strokeWidth !== 0 && !strokeWidth ) { strokeWidth = 1 }
     var el = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
-    el.setAttribute('x', Math.round(x*10) / 10);
-    el.setAttribute('y', Math.round(y*10) / 10);
+    el.setAttribute('x', roundDown(x));
+    el.setAttribute('y', roundDown(y));
     el.setAttribute('width', width);
     el.setAttribute('height', height);
     el.setAttribute('rx', borderRadius);
@@ -27,8 +27,8 @@ function generateCircle(cx, cy, r, fill, stroke, strokeWidth){
     stroke = stroke || defaultDarkColor;
     strokeWidth = strokeWidth || 1;
     var el = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
-    el.setAttribute('cx', Math.round(cx*10) / 10);
-    el.setAttribute('cy', Math.round(cy*10) / 10);
+    el.setAttribute('cx', roundDown(cx));
+    el.setAttribute('cy', roundDown(cy));
     el.setAttribute('r', r);
     el.setAttribute('fill', fill);
     el.setAttribute('stroke', stroke);
@@ -42,8 +42,8 @@ function generateText(content, x, y, fontWeight, newFontSize, fill, fontFamily, 
     fill = fill || defaultDarkColor;
     textAnchor = textAnchor || 'middle';
     var el = document.createElementNS('http://www.w3.org/2000/svg', 'text');
-    el.setAttribute('x', Math.round(x*10) / 10);
-    el.setAttribute('y', Math.round(y*10) / 10);
+    el.setAttribute('x', roundDown(x));
+    el.setAttribute('y', roundDown(y));
     el.setAttribute('font-weight', fontWeight);
     el.setAttribute('font-size', newFontSize);
     el.setAttribute('fill', fill);
@@ -59,10 +59,10 @@ function generateLine(x1, y1, x2, y2, stroke, strokeWidth){
     stroke = stroke || '#999';
     strokeWidth = strokeWidth || 1;
     var el = document.createElementNS('http://www.w3.org/2000/svg', 'line');
-    el.setAttribute('x1', Math.round(x1*10) / 10);
-    el.setAttribute('y1', Math.round(y1*10) / 10);
-    el.setAttribute('x2', Math.round(x2*10) / 10);
-    el.setAttribute('y2', Math.round(y2*10) / 10);
+    el.setAttribute('x1', roundDown(x1));
+    el.setAttribute('y1', roundDown(y1));
+    el.setAttribute('x2', roundDown(x2));
+    el.setAttribute('y2', roundDown(y2));
     el.setAttribute('stroke', stroke);
     el.setAttribute('stroke-width', strokeWidth);
     svg.append(el);
@@ -80,17 +80,27 @@ function generatePolygon(points, fill, stroke, strokeWidth){
     svg.append(el);
 };
 
-function generatePath(startx, starty, middlex, middley, endx, endy, strokeWidth, stroke, fill, isRelative){
+function generateQuadraticPath(startx, starty, middlex, middley, endx, endy, strokeWidth, stroke, fill, isRelative){
     stroke = stroke || defaultDarkColor;
     strokeWidth = strokeWidth || 1;
     fill   = fill || 'rgba(255, 255, 255, 0)';
     var relative;
     isRelative ? relative = 'q' : relative = 'Q';
-    var d = 'M' + startx + ',' + starty + ' ' + relative + middlex + ',' + middley + ' ' + endx + ',' + endy;
+    var d = 'M' + roundDown(startx) + ',' + roundDown(starty) + ' ' + relative + roundDown(middlex) + ',' + roundDown(middley) + ' ' + roundDown(endx) + ',' + roundDown(endy);
     var el = document.createElementNS('http://www.w3.org/2000/svg', 'path');
     el.setAttribute('d', d);
     el.setAttribute('stroke', stroke);
     el.setAttribute('stroke-width', strokeWidth);
     el.setAttribute('fill', fill);
+    svg.append(el);
+};
+
+function generateArc(startx, starty, radiix, radiiy, rotationx, largeArc, sweep, endx, endy, closed, stroke, fill){
+    var d = 'M' + roundDown(startx) + ',' + roundDown(starty) + ' A' + roundDown(radiix) + ',' + roundDown(radiiy) + ' ' + roundDown(rotationx) + ' ' + largeArc + ',' + sweep + ' ' + roundDown(endx) + ',' + roundDown(endy);
+    if (closed) d = d + ' z';
+    var el = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+    el.setAttribute('d', d);
+    el.setAttribute('stroke', defaultDarkColor);
+    el.setAttribute('fill', 'rgba(255, 255, 255, 0)');
     svg.append(el);
 };
---
+++ js/helper_functions.js
@@ -0,0 +1,11 @@
+function roundDown(n) {
+    return Math.round(n*10) / 10;
+}
+
+function xof(i, coordinates) {
+	return coordinates[i][0];
+}
+
+function yof(i, coordinates) {
+	return coordinates[i][1];
+}
